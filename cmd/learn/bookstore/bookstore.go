891cce566d94b2f7e9576b5fb64f681e5deb8125
qiaogaojian <qiaogaojian@vip.qq.com>
2023-01-30 18:50:47 +0800

    feat: 测试package

---
+++ cmd/learn/bookstore/bookstore.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/qiaogaojian/tweaks_go/cmd/learn/bookstore/factory"
+	"github.com/qiaogaojian/tweaks_go/cmd/learn/bookstore/memstore"
+	"github.com/qiaogaojian/tweaks_go/cmd/learn/bookstore/middleware"
+	_ "github.com/qiaogaojian/tweaks_go/cmd/learn/bookstore/server"
+	_ "github.com/qiaogaojian/tweaks_go/cmd/learn/bookstore/store"
+
+)
+
+func main() {
+
+}
